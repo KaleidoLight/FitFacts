55a391100255e232e547db37b62ec90c49c64799
Sebastiano Toniolo <sebastiano.toniolo@studenti.unipd.it>
2023-04-09 20:14:28 +0200

    rudimentale login con impostazione di drower

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fitfacts/screens/loginPage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,6 +13,44 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('FitFacts'),
       ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Expanded( 
+              child: ListView(
+                 // Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
+                children: [
+                  const UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(color: Colors.blue,),
+                    accountName: Text("Username",style: TextStyle(fontWeight: FontWeight.bold,),),
+                    accountEmail: Text("Usename.surname@gmail.com",style: TextStyle(fontWeight: FontWeight.bold,),),
+                    currentAccountPicture: Icon( Icons.account_circle, size: 100,),
+                  ),
+            
+                  ListTile(
+                    leading: Icon( Icons.home, ),
+                    title: const Text('Home Page'),
+                    onTap: () {Navigator.pop(context);},
+                  ),
+                ],
+              ),),
+            
+            Expanded( //uso due expanded così da poter posizionare il tasto di logout in fondo al drawer
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: ElevatedButton.icon(
+                  icon: Icon(Icons.exit_to_app),
+                  label: Text('LOGOUT'),
+                  onPressed: () {
+                     Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));   
+                  },
+                ),
+              ),
+            ),
+          ],),
+        ),
+      
       body: Center(
         child: Text('nothing yet'),
       ), 
