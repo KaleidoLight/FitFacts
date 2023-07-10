030b02930dec83380fffc5b89a40178d371aca1c
martina-uni <martina.zambon.7@studenti.unipd.it>
2023-07-10 10:48:16 +0200

    tolto function (calculateAge) che non usavamo su profilePage.dart

--- lib/screens/profilePage.dart
+++ lib/screens/profilePage.dart
@@ -68,26 +68,6 @@ class _ProfilePageState extends State<ProfilePage> {
 }
 
 
-/// Age Calculator
-///
-calculateAge(DateTime birthDate) {
-  DateTime currentDate = DateTime.now();
-  int age = currentDate.year - birthDate.year;
-  int month1 = currentDate.month;
-  int month2 = birthDate.month;
-  if (month2 > month1) {
-    age--;  // is the current year - 1
-  } else if (month1 == month2) {
-    int day1 = currentDate.day;
-    int day2 = birthDate.day;
-    if (day2 > day1) { // until the day before birthday
-      age--;
-    }
-  }
-  
-  return age;
-}
-
 
 /// Today Getter
 ///
